8c3bd215073305ed964a96968c800f8b89acf15b
norraVarg <wolf.se2015@gmail.com>
2024-10-16 16:49:16 +0200

    adjust css

--- src/components/ListView/ListView.tsx
+++ src/components/ListView/ListView.tsx
@@ -17,7 +17,7 @@ export const ListView = (props: Props) => {
       <div className='overflow-y-auto max-h-screen-80 md:max-h-screen-75'>
         {devices.map((device) => {
           return (
-            <Link to={`device?id=${device.id}`} state={device} key={device.id} className='h-fit transition ease-in-out duration-300 hover:bg-web-unifi-color-ublue-06 hover:bg-opacity-5 grid grid-cols-[40px_1fr_2fr] text-web-unifi-text-1 text-opacity-60 items-center h-9 border-b border-web-unifi-color-neutral-3'>
+            <Link to={`device?id=${device.id}`} state={device} key={device.id} className='py-1 h-fit transition ease-in-out duration-300 hover:bg-web-unifi-color-ublue-06 hover:bg-opacity-5 grid grid-cols-[40px_1fr_2fr] text-web-unifi-text-1 text-opacity-60 items-center h-9 border-b border-web-unifi-color-neutral-3'>
               <img src={`https://static.ui.com/fingerprint/ui/icons/${device.icon.id}_25x25.png`} />
               <span>{device.line.name}</span>
               <span>{device.product.name}</span>
